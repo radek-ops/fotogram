0efdb61e1ca562c133c03b9cf0d4933b00b341a5
radek-ops <radek.network@gmail.com>
2025-10-27 12:19:40 +0100

    Fotogram-complete

---
+++ template.js
@@ -0,0 +1,21 @@
+function dialogHtmlHeaderContend() {
+    return `<header id="headline" class="dialog_Header" onclick="closeDialogOutsite(event)" >
+    <h2 id="imgTitle"></h2>
+    <button class="close_button" type="button" onclick="buttonCloseDialog()" tabindex="0">&times;</button>
+    </header>`;
+}
+
+
+function dialogHtmlSectionContent() {
+    return `<section  onclick="closeDialogOutsite(event)"><img id="imgModal" class="lagre_img" src="" alt="" title=""></section>`;
+}
+
+
+function dialogfooterHtmlContent() {
+    return `<footer class="arrow_footer" id="dialogFooter">
+    <div  id="arrowContainer" class="arrow_Container" onclick="closeDialogOutsite(event)">
+        <button id="arrowLeft" class="arrow_button" onclick="clickButtonPrevious()" tabindex="0" type="button">&larr;</button>
+        <button id="arrowRight" class="arrow_button" onclick="clickButtonForward()" tabindex="0" type="button">&rarr;</button>
+    </div>
+</footer>`;
+}
